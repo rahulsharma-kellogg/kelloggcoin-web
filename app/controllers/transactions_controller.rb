14786cc33f50a4a4a0f5948d4d178fa9954c1a60
rahulsharma-kellogg <rahul.sharma2@kellogg.northwestern.edu>
2026-02-18 02:59:10 +0000

    All Changes

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,7 @@
 class TransactionsController < ApplicationController
   def index
-    render :template => "transactions/index"
+    # render :template => "transactions/index"
+    @transactions = Transaction.all
+    @users = User.all
   end
 end
